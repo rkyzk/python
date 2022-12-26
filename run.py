b0af166aa9d4c2cab2dd1ff86896bc90004fd6db
rkyzk <reikoyazaki@yahoo.com>
2022-12-26 12:33:17 +0000

    install APIs and set up connection to google sheets

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
-print("hello")
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('atm-system')
+
+users = SHEET.worksheet('users')
+
+data = users.get_all_values()
+
+print(data)
+        
